e089a2bb04bab5a2516c83ba4acdc86cce2dcbf6
issam jebrane <issamjebrane65@gmail.com>
2022-11-03 21:16:28 +0100

    adding files

---
+++ app.js
@@ -0,0 +1,9 @@
+function showMenuBar(){
+    let nav =document.getElementById("nav-bar-id");
+    if(nav.style.visibility == "hidden"){
+        nav.style.visibility = "visible";
+        return
+    }
+    nav.style.visibility = "hidden";
+}
+
