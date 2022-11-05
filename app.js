176c1856530c27db397b31c589afd23dbf19e641
issam jebrane <issamjebrane65@gmail.com>
2022-11-05 15:23:54 +0100

    adding eventlistener to window

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
+let nav =document.getElementById("nav-bar-id");
+let bigText = document.getElementById("big-text");
+let ids= ["nav","bigText","arrow"];
 function showMenuBar(){
-    let nav =document.getElementById("nav-bar-id");
-    if(nav.style.visibility == "hidden"){
-        nav.style.visibility = "visible";
+    if(nav.style.display == "none"){
+        nav.style.display = "flex";
+        bigText.style.display = "none";
+        arrow.style.display = "none";
         return
     }
-    nav.style.visibility = "hidden";
+    nav.style.display = "none";
+    bigText.style.display = "block";
+    arrow.style.display = "block";
 }
+window.addEventListener("resize",()=>{
+if(window.innerWidth >= "400"){
+    let navbar = document.getElementById("nav");
+    navbar.appendChild(nav)
+}
+else
+    if(window.innerWidth < "400"){
+    let header = document.getElementById("header");
+    header.appendChild(nav);
+}})
